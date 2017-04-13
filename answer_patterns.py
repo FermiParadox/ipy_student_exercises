32ff7b6e5be8fdd471b3dc9b2c84c489a29d6c1a
blue <asdasd@.22>
2017-04-13 20:49:50 +0300

    CHG: Refactoring answer checking. Probably have to change completely. CHG: Renamed  'permissions' to 'answer_patterns' ADD: Fully tested answer patterns.
    TODO: Refactor FillGap fully
    TODO: Test answer-checking fully
    TODO: In QA-widg adjust layouts, and test
    TODO: QA-widget for Y/N, and multiple-choice
    TODO: QA-widget automatically checks correct answer and displays feedback.
    TODO: Embed QA widg in a widget that contains q_history and all_achievements.

---
+++ answer_patterns.py
@@ -0,0 +1,118 @@
+"""
+Contains patterns used in order to determine if answer has expected form,
+or detect answer contents (eg. ints, decimals, etc).
+
+Sympy's `Assumptions` or `type(sympify())` not used since they sometimes produce counter-intuitive results,
+eg. `type(sympify('1/3'))` is `Pow`
+    `sympify('1+2').is_integer` is `True`
+"""
+
+
+import re
+import operator
+
+
+from arbitrary_pieces import UnexpectedValueError, print_delimiter
+
+
+PLUS_OR_MINUS_PATT = r'(?:\+|-)?'
+
+PATTERNS = []
+
+
+class _PatternBase(str):
+    _ALLOWED_OCCURRENCE_STR_OPS = {
+        r'==': operator.eq,
+        r'!=': operator.ne,
+        r'>': operator.gt,
+        r'>=': operator.ge,
+        r'<': operator.lt,
+        r'<=': operator.le,
+    }
+
+    with_sign = ''  # (added here simply for static analysis by IDE)
+
+    def __new__(cls, compile_obj):
+        try:
+            patt = compile_obj.pattern
+            inst = str.__new__(cls, patt)
+        except AttributeError:
+            # (`re.compile` objects contain `.pattern`)
+            raise UnexpectedValueError('`compile_obj` must be a `re.compile` object.')
+        if ('_' in patt) or (' ' in patt):
+            raise UnexpectedValueError('Pattern must not include whitespaces or "_".')
+        inst.__dict__.update({'with_sign': PLUS_OR_MINUS_PATT + inst})
+        _PatternBase._check_duplicates_and_note_new_pattern(pattern=patt)
+        return inst
+
+    @staticmethod
+    def total_matches_within_bounds(m, bounds_str):
+        """
+        Checks if total matches found meet the criteria of `bounds_str`.
+
+        :param bounds_str: Expresses bounds of total matches found (must contain 'm').
+            eg. 'm>2', 'm!=3', '2<=m<3'
+        :return: (bool)
+        """
+        for k in _PatternBase._ALLOWED_OCCURRENCE_STR_OPS:
+            match = re.fullmatch('m({})(\d+)'.format(k), bounds_str)
+            if match:
+                op, num = match.group(1), int(match.group(2))
+                return _PatternBase._ALLOWED_OCCURRENCE_STR_OPS[op](m, num)
+        for k1 in _PatternBase._ALLOWED_OCCURRENCE_STR_OPS:
+            for k2 in _PatternBase._ALLOWED_OCCURRENCE_STR_OPS:
+                match = re.fullmatch('(\d+)({k1})m({k2})(\d+)'.format(k1=k1, k2=k2), bounds_str)
+                if match:
+                    num1, op1, op2, num2 = int(match.group(1)), match.group(2), match.group(3), int(match.group(4))
+                    within_1st_bound = _PatternBase._ALLOWED_OCCURRENCE_STR_OPS[op1](num1, m)
+                    within_2nd_bound = _PatternBase._ALLOWED_OCCURRENCE_STR_OPS[op2](m, num2)
+                    return within_1st_bound and within_2nd_bound
+        else:
+            raise UnexpectedValueError('Occurrence string {} not matching requirements.'.format(bounds_str))
+
+    @staticmethod
+    def _check_duplicates_and_note_new_pattern(pattern):
+        existing_patterns = [i for i in PATTERNS]
+        if pattern in existing_patterns:
+            raise UnexpectedValueError('Following pattern exists more than once: {}'.format(pattern))
+        else:
+            PATTERNS.append(pattern)
+
+    @staticmethod
+    def findall(compile_obj, expr, bounds_str):
+        matches = re.findall(compile_obj, expr)
+        tot_matches_found = len(matches)
+        return _PatternBase.total_matches_within_bounds(m=tot_matches_found, bounds_str=bounds_str)
+
+
+find_m_patterns = _PatternBase.findall
+
+
+# TEMPLATE
+"""
+? = _PatternBase(re.compile(r'\'))
+"""
+
+
+# (Using `re.compile` for visibility provided by IDE)
+# Patterns must account for the fact that variables might be named 'x1'.
+integer = _PatternBase(re.compile(r'(?<![a-zA-Z0-9.])\d+(?!\.)'))
+decimal = _PatternBase(re.compile(r'(?<![a-zA-Z0-9.])\d+\.\d+'))
+fraction_of_ints = _PatternBase(
+    re.compile(r'(?:{i}/{i})|(?:\({s}{i}\)/{i})|(?:{i}/\({s}{i}\))|(?:\({s}{i}\)/\({s}{i}\))'.format(i=integer,
+                                                                                                     s=PLUS_OR_MINUS_PATT)))
+
+
+if __name__ == '__main__':
+
+    print_delimiter()
+    print('PATTERNS AND OBJ:\n')
+    for p in PATTERNS:
+        print('{}'.format(p))
+
+    print_delimiter()
+    print(integer)
+    print(re.fullmatch(integer, '2'))
+    print(re.fullmatch(integer, '+2'))
+    print(re.fullmatch(integer.with_sign, '+2'))
+
---
+++ tests/test_answer_patterns.py
@@ -0,0 +1,178 @@
+from unittest import TestCase
+from re import compile, fullmatch
+
+
+import answer_patterns
+
+from answer_patterns import _PatternBase, PATTERNS
+from arbitrary_pieces import UnexpectedValueError
+
+
+# ---------------------------------------------------------------------------------
+class Test_total_matches_within_bounds(TestCase):
+    WITHIN_BOUNDS_INPUTS = {'m>2': 3,
+                            'm<=1': 1,
+                            'm>=10': 111,
+                            'm==45': 45,
+                            'm!=5': 2,
+                            '2<=m<15': 3}
+    OUT_OF_BOUNDS_INPUTS = {'m>2': 2,
+                            'm<=1': 34,
+                            'm>=10': 8,
+                            'm==45': 2,
+                            'm!=5': 5,
+                            '2<=m<15': 1}
+    INVALID_INPUTS = {'2',
+                      '<=',
+                      '10>',
+                      '2>1',
+                      '4==4',
+                      '4!=5',
+                      '>x',
+                      '2<x<4',
+                      '1.3<m<4'}
+
+    def test_within_bounds(self):
+        for k, v in self.WITHIN_BOUNDS_INPUTS.items():
+            self.assertTrue(_PatternBase.total_matches_within_bounds(m=v, bounds_str=k))
+
+    def test_out_of_bounds(self):
+        for k, v in self.OUT_OF_BOUNDS_INPUTS.items():
+            self.assertFalse(_PatternBase.total_matches_within_bounds(m=v, bounds_str=k),
+                             msg='m: {}, Pattern: {}'.format(v, k))
+
+    def test_invalid_input(self):
+        for i in self.INVALID_INPUTS:
+            from random import randint
+            self.assertRaises(UnexpectedValueError, _PatternBase.total_matches_within_bounds, randint(1, 1000), i)
+
+
+class Test__check_duplicates_and_note_new_pattern(TestCase):
+    def test_duplicate_detected(self):
+        _PatternBase(compile(r'1'))
+        self.assertRaises(UnexpectedValueError, _PatternBase, compile(r'1'))
+
+    def test_noted_new_pattern(self):
+        inst = _PatternBase(compile(r'x'))
+        # Must be in either key or val
+        self.assertIn(inst, PATTERNS)
+
+
+class Test_PatternBase(TestCase):
+    def test_str_not_accepted(self):
+        self.assertRaises(UnexpectedValueError, _PatternBase, r'\d+')
+        self.assertRaises(UnexpectedValueError, _PatternBase, '\d+')
+
+
+# ---------------------------------------------------------------------------------
+class TestEachPattern(TestCase):
+    # fullmatch test-base
+    def _fullmatch_base_(self, lst_of_strs, search_patt, _y_n):
+        assert_func = self.assertTrue if (_y_n == 'y') else self.assertFalse
+        for s in lst_of_strs:
+            assert_func(fullmatch(search_patt, s),
+                        'Pattern: {} \nnot found in: {}'.format(search_patt, s))
+
+    def _does_fullmatch(self, lst_of_strs, pattern):
+        return self._fullmatch_base_(lst_of_strs=lst_of_strs, search_patt=pattern, _y_n='y')
+
+    def _does_not_fullmatch(self, lst_of_strs, pattern):
+        return self._fullmatch_base_(lst_of_strs=lst_of_strs, search_patt=pattern, _y_n='n')
+
+    # find_m_patterns test-base
+    def _find2patterns_base(self, bounds_strs_lst, searched_patt, _y_n):
+        m = 2
+        m_str = 'm==2'
+        assert_func = self.assertTrue if (_y_n == 'y') else self.assertFalse
+        for expr in bounds_strs_lst:
+            assert_func(answer_patterns.find_m_patterns(compile_obj=searched_patt, expr=expr, bounds_str=m_str),
+                        'Did not find {} times \nthe pattern: {} \nin the string: {}'.format(m, searched_patt, expr))
+
+    def _does_find2patterns(self, bounds_strs_lst, pattern):
+        return self._find2patterns_base(bounds_strs_lst=bounds_strs_lst, searched_patt=pattern, _y_n='y')
+
+    def _does_not_find2patterns(self, bounds_strs_lst, pattern):
+        return self._find2patterns_base(bounds_strs_lst=bounds_strs_lst, searched_patt=pattern, _y_n='n')
+
+    # TEMPLATE: fullmatch
+    """
+    # ?
+    def test_fullmatch_?(self):
+        self._does_fullmatch(lst_of_strs=[?],
+                             pattern=answer_patterns.?)
+
+    def test_not_fullmatch_?(self):
+        self._does_not_fullmatch(lst_of_strs=[?],
+                                 pattern=answer_patterns.?)
+
+    """
+
+    # TEMPLATE: find_m_patterns
+    """
+    def test_find2patterns_?(self):
+        lst = [?]
+        self._does_find2patterns(bounds_strs_lst=lst,
+                           pattern=answer_patterns.?)
+
+    def test_not_find2patterns_?(self):
+        lst = [?]
+        self._does_not_find2patterns(bounds_strs_lst=lst,
+                               pattern=answer_patterns.?)
+    """
+
+    # integer
+    def test_fullmatch_integer(self):
+        self._does_fullmatch(lst_of_strs=['123', '-24', '0', '-0', '+0', '+2'],
+                             pattern=answer_patterns.integer.with_sign)
+
+    def test_not_fullmatch_integer(self):
+        self._does_not_fullmatch(lst_of_strs=['93.2', '2.0', '.4', '-1/3', '-2x', '4*2'],
+                                 pattern=answer_patterns.integer)
+
+    def test_find2patterns_integer(self):
+        lst = ['1*z+2+1.4', '144+0.1/1000-2.4']
+        self._does_find2patterns(bounds_strs_lst=lst,
+                                 pattern=answer_patterns.integer)
+
+    def test_not_find2patterns_integer(self):
+        lst = ['2+4+3', '1.73-4/2+7']
+        self._does_not_find2patterns(bounds_strs_lst=lst,
+                                     pattern=answer_patterns.integer)
+
+    # decimal
+    def test_fullmatch_decimal(self):
+        self._does_fullmatch(lst_of_strs=['1.3', '0.004', '-1888.2', '+488.2', '0.0', '-0.0', '+0.0'],
+                             pattern=answer_patterns.decimal.with_sign)
+
+    def test_not_fullmatch_decimal(self):
+        self._does_not_fullmatch(lst_of_strs=['1', '1422', '0', '.4', '4.', '4.2*x', '4.2-3.1', '0x'],
+                                 pattern=answer_patterns.decimal.with_sign)
+
+    def test_find2patterns_decimal(self):
+        lst = ['-1.4+1145.552', '(4.5*x/2)**0.001']
+        self._does_find2patterns(bounds_strs_lst=lst,
+                                 pattern=answer_patterns.decimal)
+
+    def test_not_find2patterns_decimal(self):
+        lst = ['-1+1145.552', '.5+0.1']
+        self._does_not_find2patterns(bounds_strs_lst=lst,
+                                     pattern=answer_patterns.decimal)
+
+    # fraction_of_ints
+    def test_fullmatch_fraction_of_ints(self):
+        self._does_fullmatch(lst_of_strs=['-1/3', '(+2)/4', '(-95)/(+34)'],
+                             pattern=answer_patterns.fraction_of_ints.with_sign)
+
+    def test_not_fullmatch_fraction_of_ints(self):
+        self._does_not_fullmatch(lst_of_strs=['2/3.0', '-x/2', '1.2'],
+                                 pattern=answer_patterns.fraction_of_ints.with_sign)
+
+    def test_find2patterns_fraction_of_ints(self):
+        lst = ['1/3+4/20*0.1', '400/2-200/777', '(-10)/(+4)-4/20']
+        self._does_find2patterns(bounds_strs_lst=lst,
+                                 pattern=answer_patterns.fraction_of_ints.with_sign)
+
+    def test_not_find2patterns_fraction_of_ints(self):
+        lst = ['1/2.3+1/4', '(-1)/2/4']
+        self._does_not_find2patterns(bounds_strs_lst=lst,
+                                     pattern=answer_patterns.fraction_of_ints.with_sign)
